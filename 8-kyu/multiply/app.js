79688978f7b82a94959724fa93db062a572b948e
andrewmarich <marich.andrew@outlook.com>
2023-01-01 12:58:53 -0700

    Coding challenge 01-01-23

---
+++ 8-kyu/multiply/app.js
@@ -0,0 +1,19 @@
+//This code does not execute properly. Try to figure out why.
+
+//P
+//a, b
+
+//R
+//a * b
+
+//E
+multiply(1,1) //1
+multiply(2,1) //2
+multiply(2,2) //4
+
+//P
+//missing return
+
+function multiply(a, b){
+  return a * b
+}
