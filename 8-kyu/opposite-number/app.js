3435d9022d31b60567ab72266d1f793d781a86e4
andrewmarich <marich.andrew@outlook.com>
2023-01-03 13:02:10 -0700

    Coding challenge 01-03-23

---
+++ 8-kyu/opposite-number/app.js
@@ -0,0 +1,19 @@
+//Very simple, given an integer or a floating-point number, find its opposite.
+
+//P
+//integer or floating num
+
+//R
+//reversed num
+
+//E
+opposite(1) //1
+opposite(0) //0
+opposite(4.25) //-4.25
+
+//P
+//return -num
+
+function opposite(number) {
+  return -number
+}
