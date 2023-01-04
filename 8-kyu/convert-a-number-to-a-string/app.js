f95802f8096d8573d274b61a4add41cfb78ba4d5
andrewmarich <marich.andrew@outlook.com>
2023-01-04 13:02:54 -0700

    Coding challenge 01-04-23

---
+++ 8-kyu/convert-a-number-to-a-string/app.js
@@ -0,0 +1,17 @@
+//We need a function that can transform a number (integer) into a string.
+
+//P
+//integer
+
+//R
+//string
+
+//E
+numberToString(67) //'67'
+
+//P
+//String method
+
+function numberToString(num) {
+  return String(num)
+}
