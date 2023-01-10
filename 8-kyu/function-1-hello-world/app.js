ee2d9fb42457d25ff952a837b70dd390ebb2cf0e
andrewmarich <marich.andrew@outlook.com>
2023-01-10 13:09:40 -0700

    Coding challenge 01-10-23

---
+++ 8-kyu/function-1-hello-world/app.js
@@ -0,0 +1,17 @@
+//Make a simple function called greet that returns the most-famous "hello world!".
+
+//P
+//none
+
+//R
+//'hello world!'
+
+//E
+greet() //'hello world!'
+
+//P
+//return 'hello world!''
+
+function greet () {
+  return 'hello world!'
+}
