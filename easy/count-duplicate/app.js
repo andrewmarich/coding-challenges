8b683d08836a0faae702fcb7ea165f851045f311
andrewmarich <marich.andrew@outlook.com>
2023-09-26 14:34:22 -0700

    Coding challenge 09-26-23

--- easy/count-duplicate/app.js
+++ easy/count-duplicate/app.js
@@ -3,12 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] === nums[j]) {
-                return true
-            }
+    let set = new Set()
+    for (let num of nums) {
+        if (set.has(num)) {
+            return true
         }
+        set.add(num)
     }
     return false
 };
