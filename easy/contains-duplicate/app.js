1115ae63be8f75ccb8be0d8021fbe9d5e7f7ef2f
andrewmarich <marich.andrew@outlook.com>
2023-10-01 12:22:58 -0700

    Coding challenge 10-01-23

--- easy/contains-duplicate/app.js
+++ easy/contains-duplicate/app.js
@@ -3,12 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] === nums[j]) {
-                return true
-            }
-        }
-    }
-    return false
+    let sorted = nums.sort((a,b) => a-b)
+    return sorted.some((e, i) => e === sorted[i+1])
 };
