8e61d8ca2301076eaed157fd92292f320179de9c
andrewmarich <marich.andrew@outlook.com>
2023-10-05 12:53:52 -0700

    Coding challenge 10-05-23

--- easy/two-sum/app.js
+++ easy/two-sum/app.js
@@ -4,12 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = new Map();
-
     for (let i = 0; i < nums.length; i++) {
-        if (map.has(target - nums[i])) {
-            return [map.get(target - nums[i]), i]
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
         }
-        map.set(nums[i], i)
     }
 };
