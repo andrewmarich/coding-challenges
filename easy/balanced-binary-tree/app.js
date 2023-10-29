65c1037fb8116eeed6a779d75e515576128ab768
andrewmarich <marich.andrew@outlook.com>
2023-10-29 11:54:19 -0700

    Coding challenge 10-29-23

--- easy/balanced-binary-tree/app.js
+++ easy/balanced-binary-tree/app.js
@@ -11,20 +11,17 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    if (!root) return true;
-    if (dfs(root) === -1) return false;
 
-    return true;
+    let res = true;
 
-    function dfs(root) {
-        if (!root) return 0;
-
-        let left = dfs(root.left), right = dfs(root.right);
-
-        if (left === -1 || right === -1) return -1;
-
-        if (Math.abs(left - right) > 1) return -1;
-
+    function dfs(node) {
+        if (!node) return 0;
+        let left = dfs(node.left), right = dfs(node.right);
+        if (Math.abs(left - right) > 1) res = false;
         return Math.max(left, right) + 1;
     }
+
+    dfs(root);
+
+    return res;
 };
