7068fc728337da344343f162a839e2315e4bd5a1
andrewmarich <marich.andrew@outlook.com>
2023-11-07 11:07:22 -0700

    Coding challenge 11-07-23

--- python/easy/two-sum/app.py
+++ python/easy/two-sum/app.py
@@ -1,8 +1,11 @@
+# Two Sum: Two-Pass Hash
+
 class Solution:
-    
-    # Brute Force
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range (len(nums) - 1):
-            for j in range (i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        map = {}
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in map and map[diff] != i:
+                return [i, map[diff]]
+            map[nums[i]] = i
