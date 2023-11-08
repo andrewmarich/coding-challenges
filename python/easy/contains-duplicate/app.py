1742689b055ae26bb318980b5e70515f05b2382c
andrewmarich <marich.andrew@outlook.com>
2023-11-08 11:11:45 -0700

    Coding challenge 11-08-23

--- python/easy/contains-duplicate/app.py
+++ python/easy/contains-duplicate/app.py
@@ -1,9 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashset = set()
-
-        for num in nums:
-            if num in hashset:
-                return True
-            hashset.add(num)
+        nums = sorted(nums)
+        
+        for i in range (len(nums) - 1):
+            for j in range (i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
         return False
