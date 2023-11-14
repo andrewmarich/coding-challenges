16effb52a127f7c374a98450c19076a6205bdc58
andrewmarich <marich.andrew@outlook.com>
2023-11-14 15:58:18 -0700

    Coding challenge 11-14-23

--- python/easy/valid-parentheses/app.py
+++ python/easy/valid-parentheses/app.py
@@ -1,15 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
+        closeToOpen = {')' : '(', '}' : '{', ']' : '['}
 
         for c in s:
             if c in closeToOpen:
                 if stack and stack[-1] == closeToOpen[c]:
                     stack.pop()
-                else: 
+                else:
                     return False
             else:
                 stack.append(c)
-        
-        return True
+
+        return True if not stack else False
