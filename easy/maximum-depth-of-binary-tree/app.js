83eef3664b15f0508179a50dce503aae492d4c71
andrewmarich <marich.andrew@outlook.com>
2023-12-10 13:45:33 -0700

    Coding challenge 12-10-23

--- easy/maximum-depth-of-binary-tree/app.js
+++ easy/maximum-depth-of-binary-tree/app.js
@@ -1,30 +1,19 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
 var maxDepth = function(root) {
-    if (!root) return 0;
+    if(!root) return 0;
 
     //BFS (level order)
 
-    let level = 0; queue = [];
+    let level = 0, queue = [];
     queue.push(root);
-
-    while (queue.length > 0) {
-        for (let i = 0; i < queue.length; i++) {
+    
+    while(queue.length > 0){
+        let count = queue.length;
+        
+        for(let i = 0; i < count; i++){
             const node = queue.shift();
+            if(node.right) queue.push(node.right);
+            if(node.left) queue.push(node.left);
             
-            if (node.right) queue.push(node.right);
-            if (node.left) queue.push(node.left);
-
         }
         level++;
     }
