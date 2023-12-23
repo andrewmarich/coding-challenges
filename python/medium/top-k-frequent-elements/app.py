1f3c416847ccc0f94da2a2b9bed1d1beabfc0982
andrewmarich <marich.andrew@outlook.com>
2023-12-23 15:28:19 -0700

    Coding challenge 12-23-23

--- python/medium/top-k-frequent-elements/app.py
+++ python/medium/top-k-frequent-elements/app.py
@@ -1,16 +1,25 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #map to store the count of each unique number
         count = {}
+        #list to store lists of numbers based on their frequencies
         freq = [[] for i in range(len(nums) + 1)]
 
+        #iterates through nums and increments count of num
         for n in nums:
             count[n] = 1 + count.get(n, 0)
-        for n, c in count.items():
-            freq[c].append(n)
+
+        #iterates through count dict and groups nums based on freq
+        for n, i in count.items():
+            freq[i].append(n)
 
         res = []
+        #decrements through freq list
         for i in range(len(freq) - 1, 0, -1):
+            #iterates through list of nums and appends to res
             for n in freq[i]:
                 res.append(n)
-                if len(res) == k:
-                    return res
+            if len(res) == k:
+                return res
+
+        
