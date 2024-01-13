73fc5c1b1203a92578315aa54f91e70c70f1d5c5
andrewmarich <marich.andrew@outlook.com>
2024-01-13 13:40:59 -0700

    Coding challenge 1-13-24

--- python/medium/3sum/app.py
+++ python/medium/3sum/app.py
@@ -6,18 +6,17 @@ class Solution:
         for i, a in enumerate(nums):
             if i > 0 and a == nums[i - 1]:
                 continue
-
+            
             left, right = i + 1, len(nums) - 1
             while left < right:
                 threeSum = a + nums[left] + nums[right]
-                if threeSum == 0:
+                if threeSum > 0:
+                    right -= 1
+                elif threeSum < 0:
+                    left += 1
+                else:
                     res.append([a, nums[left], nums[right]])
                     left += 1
                     while nums[left] == nums[left - 1] and left < right:
                         left += 1
-                elif threeSum < 0:
-                    left += 1
-                else:
-                    right -= 1
-
         return res
