2dcbbc88de1487a086e8ded1002f4804f0560d52
andrewmarich <marich.andrew@outlook.com>
2024-01-18 13:45:20 -0700

    Coding challenge 1-18-24

--- python/easy/two-sum/app.py
+++ python/easy/two-sum/app.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}
+        map = {}
 
         for i, n in enumerate(nums):
             diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
+            if diff in map:
+                return [map[diff], i]
+            map[n] = i
