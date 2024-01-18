a4a07c95bd18c783df2983db3a97d680e000e63e
andrewmarich <marich.andrew@outlook.com>
2024-01-17 18:19:19 -0700

    Coding challenge 1-17-24

--- python/easy/contains-duplicate/app.py
+++ python/easy/contains-duplicate/app.py
@@ -1,3 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
+        seen = {}
+
+        for num in nums:
+            if num in seen:
+                return True
+                
+            else:
+                seen[num] = num
+
+        return False
