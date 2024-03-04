559b5997afe624dc0fc8be17562501e24b992794
andrewmarich <marich.andrew@outlook.com>
2024-03-04 14:09:54 -0700

    Coding challenge 3-4-24

--- python/easy/reverse-linked-list/app.py
+++ python/easy/reverse-linked-list/app.py
@@ -7,10 +7,9 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev, curr = None, head
 
-        while curr: 
+        while curr:
             nxt = curr.next
             curr.next = prev
             prev = curr
             curr = nxt
         return prev
-        
