98d76076568bfc6b76a25a1c0eb331bf921eab5e
andrewmarich <marich.andrew@outlook.com>
2024-03-17 13:17:53 -0700

    Coding challenge 1-5-24

--- python/medium/top-k-frequent-elements/app.py
+++ python/medium/top-k-frequent-elements/app.py
@@ -10,9 +10,8 @@ class Solution:
             freq[i].append(n)
 
         res = []
-        for i in range(len(freq) - 1, 0, -1):
+        for i in range(len(freq) -1, 0, -1):
             for n in freq[i]:
                 res.append(n)
             if len(res) == k:
                 return res
-            
