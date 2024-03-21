f070ac5b21d049c675693012ce1dbb610fac0dde
andrewmarich <marich.andrew@outlook.com>
2024-03-21 14:04:41 -0700

    Coding challenge 3-21-24

--- python/medium/product-of-array-except-self/app.py
+++ python/medium/product-of-array-except-self/app.py
@@ -1,15 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * len(nums)
+        answer = [1] * len(nums)
 
         prefix = 1
-        for i in range(len(nums) - 1):
-            res[i] *= prefix
+        for i in range(len(nums)):
+            answer[i] *= prefix
             prefix *= nums[i]
 
         postfix = 1
         for i in reversed(range(len(nums))):
-            res[i] *= postfix
+            answer[i] *= postfix
             postfix *= nums[i]
 
-        return res
+        return answer
