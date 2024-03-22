cda56a12902218b73aa0f6d58a3c18e82d00eba8
andrewmarich <marich.andrew@outlook.com>
2024-03-22 14:14:23 -0700

    Coding challenge 3-22-24

--- python/medium/valid-sudoku/app.py
+++ python/medium/valid-sudoku/app.py
@@ -7,5 +7,5 @@ class Solution:
                 ele = board[row][col]
                 if ele != ".":
                     res += [(row, ele), (ele, col), (row // 3, col // 3, ele)]
-
+        
         return len(res) == len(set(res))
