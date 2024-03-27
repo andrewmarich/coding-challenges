dffe8d2073104da71365b0e2411af70ce689fb8f
andrewmarich <marich.andrew@outlook.com>
2024-03-26 21:57:40 -0700

    Coding challenge 3-26-24

--- python/medium/two-sum-ii-input-array-is-sorted/app.py
+++ python/medium/two-sum-ii-input-array-is-sorted/app.py
@@ -2,11 +2,11 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
         while left < right:
-            sum = numbers[left] + numbers[right]
-            if sum == target:
+            twoSum = numbers[left] + numbers[right]
+            if twoSum == target:
                 return [left + 1, right + 1]
-            elif sum < target:
+            elif twoSum < target:
                 left += 1
             else:
-                right -= 1
+                right -=1
         return []
