3b22436c22cb20b22ecbae0ea52688a4d76bf052
andrewmarich <marich.andrew@outlook.com>
2024-05-31 15:47:27 -0700

    Coding challenge 5-31-24

--- python/medium/group-anagrams/app.py
+++ python/medium/group-anagrams/app.py
@@ -1,13 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
+        map = defaultdict(list)
 
         for s in strs:
-            count = [0] * 26
+            sortedStr = ''.join(sorted(s))
+            map[sortedStr].append(s)
 
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-
-            res[tuple(count)].append(s)
-
-        return res.values()
+        return list(map.values())
