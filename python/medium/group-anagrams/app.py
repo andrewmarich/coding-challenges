b7115af6bb42e08f12971376ae61190c97173b6b
andrewmarich <marich.andrew@outlook.com>
2024-06-04 15:57:07 -0700

    Coding challenge 6-4-24

--- python/medium/group-anagrams/app.py
+++ python/medium/group-anagrams/app.py
@@ -2,8 +2,8 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         map = defaultdict(list)
 
-        for str in strs:
-            sortedStr = ''.join(sorted(str))
-            map[sortedStr].append(str)
+        for s in strs:
+            sortedStr = ''.join(sorted(s))
+            map[sortedStr].append(s)
 
         return list(map.values())
