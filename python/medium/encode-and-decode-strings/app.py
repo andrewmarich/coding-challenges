2d2a4c66f5cbb9d2a9eea101dcc4200c304b9248
andrewmarich <marich.andrew@outlook.com>
2024-06-06 15:46:29 -0700

    Coding challenge 6-6-24

--- python/medium/encode-and-decode-strings/app.py
+++ python/medium/encode-and-decode-strings/app.py
@@ -1,12 +1,14 @@
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
-        res = ""
+        res = ''
         for s in strs:
             res += str(len(s)) + "#" + s
         return res
+
     def decode(self, s: str) -> List[str]:
         res, i = [], 0
+
         while i < len(s):
             j = i
             while s[j] != "#":
@@ -15,4 +17,3 @@ class Solution:
             res.append(s[j + 1 : j + 1 + length])
             i = j + 1 + length
         return res
-            
