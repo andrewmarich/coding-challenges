054d44ee3d29d7ad20f46e4018efec16e1d6ba73
andrewmarich <marich.andrew@outlook.com>
2024-06-17 14:49:54 -0700

    Coding challenge 6-17-24

--- python/easy/valid-palindrome/app.py
+++ python/easy/valid-palindrome/app.py
@@ -1,10 +1,9 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        res = []
+        newStr = ""
 
         for c in s:
             if c.isalnum():
-                res.append(c.lower())
+                newStr += c.lower()
 
-        newStr = ''.join(res)
         return newStr == newStr[::-1]
