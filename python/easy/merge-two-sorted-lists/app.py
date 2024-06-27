b780daa8a5d6d6ea846766509bef8a6ea2098dbd
andrewmarich <marich.andrew@outlook.com>
2024-06-27 13:33:31 -0700

    Coding challenge 6-27-24

--- python/easy/merge-two-sorted-lists/app.py
+++ python/easy/merge-two-sorted-lists/app.py
@@ -5,17 +5,21 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        curr = dummy = ListNode()
+        dummy = ListNode()
+        tail = dummy
 
         while list1 and list2:
             if list1.val < list2.val:
-                curr.next = list1
-                list1, curr = list1.next, list1
-            else:
-                curr.next = list2
-                list2, curr = list2.next, list2
+                tail.next = list1
+                list1 = list1.next
+            else: 
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
 
-        if list1 or list2:
-            curr.next = list1 if list1 else list2
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
 
         return dummy.next
