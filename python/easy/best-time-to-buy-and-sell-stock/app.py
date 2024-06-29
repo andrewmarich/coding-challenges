98b3d3a97798b42fd443370d39c2bbdfd597f7df
andrewmarich <marich.andrew@outlook.com>
2024-06-29 13:37:37 -0700

    Coding challenge 6-29-24

--- python/easy/best-time-to-buy-and-sell-stock/app.py
+++ python/easy/best-time-to-buy-and-sell-stock/app.py
@@ -3,7 +3,7 @@ class Solution:
         l, r = 0, 1
         maxP = 0
 
-        while r > len(prices):
+        while r < len(prices):
             if prices[r] > prices[l]:
                 profit = prices[r] - prices[l]
                 maxP = max(maxP, profit)
