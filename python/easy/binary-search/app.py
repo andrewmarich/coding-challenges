aaa74104d05a56090222a18af72322114984f2de
andrewmarich <marich.andrew@outlook.com>
2024-07-17 13:21:24 -0700

    Coding challenge 7-17-24

--- python/easy/binary-search/app.py
+++ python/easy/binary-search/app.py
@@ -1,7 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
-        
+
         while l <= r:
             mid = (l + r) // 2
             if nums[mid] > target:
@@ -9,5 +9,6 @@ class Solution:
             elif nums[mid] < target:
                 l = mid + 1
             else:
-                return mid     
+                return mid
+
         return -1
